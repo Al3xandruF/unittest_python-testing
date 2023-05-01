1be50f46ea4c223fe630035dabd24dfb9193d4c8
Al3xandruF <al3x.florea@gmail.com>
2023-05-01 11:05:29 +0000

    Create test_alert_santa

--- test_student.py
+++ test_student.py
@@ -10,5 +10,12 @@ class TestStudent(unittest.TestCase):
         self.assertEqual(student.full_name, 'Alex Florea')
 
 
+    def test_alert_santa(self):
+        student = Student('Alex', 'Florea')
+        student.alert_santa()
+
+        self.assertTrue(student.naughty_list)
+
+
 if __name__ == '__main__':
     unittest.main()
